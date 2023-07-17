c3f391db02c5460dc02e1f28744c63bd3734aa33
Hiskiyas <hiskiyasf@gmail.com>
2023-07-17 16:13:24 +0300

    Made The last digit suitable for negative numbers

---
+++ python-hello_world/3-last_digit.py
@@ -0,0 +1,12 @@
+import random
+number = random.randint(-10000, 10000)
+if number >= 0:
+    last_dig = number%10
+else:
+    last_dig = number%-10
+if last_dig > 5:
+    print("Last digit of " + str(number)+ " is " +str(last_dig) + " and is greater than 5")
+if last_dig == 0:
+    print("Last digit of " + str(number)+ " is " +str(last_dig) + " and is 0")
+if last_dig < 6:
+    print("Last digit of " + str(number)+ " is " +str(last_dig) + " and is less than 6 and not 0")
---
+++ python-hello_world/try.py
@@ -0,0 +1,3 @@
+num = -18
+num2 = num%-10
+print(str(num2))
