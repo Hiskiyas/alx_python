ab4835acd68c8c849c64c40933a03a2ea89574d1
Hiskiyas <hiskiyasf@gmail.com>
2023-07-19 11:39:19 +0300

    New Method Indroduced

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,8 +1,8 @@
 import random
 number = random.randint(-10000, 10000)
-last_dig = number%10
+last_dig = abs(number)%10
 if number < 0 :
-    last_dig = number%-10
+    last_dig = -last_dig
 if last_dig > 5:
         print("Last digit of " + str(number)+ " is " +str(last_dig) + " and is greater than 5",end="\n")
 elif last_dig == 0:
