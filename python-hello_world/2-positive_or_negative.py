086359164608a54e2e823a8330d16e4d5fd38c92
Hiskiyas <hiskiyasf@gmail.com>
2023-07-19 10:38:36 +0300

    New Line Introduced For Number 2

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,8 +1,8 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print(str(number) + " is positive")
+    print(str(number) + " is positive",end="\n")
 if number == 0:
-    print(str(number) + " is zero")
+    print(str(number) + " is zero",end="\n")
 if number < 0:
-    print(str(number) + " is negative")
+    print(str(number) + " is negative",end="\n")
