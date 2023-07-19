3234dfac6b907e53e11462c1570fccc18ef97396
Hiskiyas <hiskiyasf@gmail.com>
2023-07-19 08:39:07 +0300

    Last Digit not zero edited

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -8,5 +8,5 @@ if last_dig > 5:
     print("Last digit of " + str(number)+ " is " +str(last_dig) + " and is greater than 5")
 if last_dig == 0:
     print("Last digit of " + str(number)+ " is " +str(last_dig) + " and is 0")
-if last_dig < 6:
+if last_dig < 6 and last_dig != 0:
     print("Last digit of " + str(number)+ " is " +str(last_dig) + " and is less than 6 and not 0")
