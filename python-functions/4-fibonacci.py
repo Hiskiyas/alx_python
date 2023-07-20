6282cf18729153303673c508c84dcc800d7a0b1a
Hiskiyas <hiskiyasf@gmail.com>
2023-07-20 16:32:27 +0300

    Some Adjustments on Fibonacci Series

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 def fibonacci_sequence(n):
     if n == 0:
-          fb=[]
-          return fb
+      fb=[]
+      return fb
     elif n==1:
-          fb=[0]
-          return fb
+      fb=[0]
+      return fb
     else: 
-         fb = [0,1]
+      fb = [0,1]
     for i in range(2, n):
                 fb.append(fb[i-1] + fb[i-2])
     return fb
