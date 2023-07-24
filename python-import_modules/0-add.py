2116a79456b1ef750e42b4ce2ca71d9e44485a0d
Hiskiyas <99412951+Hiskiyas@users.noreply.github.com>
2023-07-24 22:03:51 +0300

    Few Adjustments

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,6 +2,6 @@
 from add_0 import add
 a = 1
 b = 2
-result = add(a,b)
-if __name__=="__main__":
-    print("{} + {} = {}".format(str(a),str(b),str(result)), end="\n")
+result = add(a, b)
+if __name__ == "__main__":
+    print("{} + {} = {}".format(str(a), str(b), str(result)), end="\n")
