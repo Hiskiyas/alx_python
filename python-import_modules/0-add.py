12c311460c975b1b4350e2fdd352292d21718db9
Hiskiyas <99412951+Hiskiyas@users.noreply.github.com>
2023-07-24 21:48:51 +0300

    script Will not run on other imported functions

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,4 +3,5 @@ from add_0 import add
 a = 1
 b = 2
 result = add(a,b)
-print("{} + {} = {}".format(str(a),str(b),str(result)), end="\n")
+if __name__=="__main__":
+    print("{} + {} = {}".format(str(a),str(b),str(result)), end="\n")
