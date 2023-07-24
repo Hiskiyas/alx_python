05df91cace6982fdcb928089289075f23ee6ec57
Hiskiyas <99412951+Hiskiyas@users.noreply.github.com>
2023-07-24 22:42:52 +0300

    File changed to script mode

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3 
 from sys import argv
 def print_arguments():
+  if __name__ == "__main__":
     num_arguments = len(argv) - 1
     if num_arguments == 0:
         print(num_arguments, "arguments.")
