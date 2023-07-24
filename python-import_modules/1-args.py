6b1500ed366e84790df9a751c432d5dee45bbaed
Hiskiyas <99412951+Hiskiyas@users.noreply.github.com>
2023-07-24 22:52:36 +0300

    Indentations Fixed For Four multiples

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,14 +1,14 @@
-#!/usr/bin/python3 
+#!/usr/bin/ python3 
 from sys import argv
 def print_arguments():
-  if __name__ == "__main__":
-    num_arguments = len(argv) - 1
-    if num_arguments == 0:
-        print(num_arguments, "arguments.")
-    elif num_arguments == 1:
-        print(num_arguments, "argument:")
-    else:
-        print(num_arguments, "arguments:")
-    for i in range(1,len(argv)):
-            print("{}: {}".format(i,argv[i]))
-print_arguments()
+    if __name__ == "__main__":
+        num_arguments = len(argv) - 1
+        if num_arguments == 0:
+            print(num_arguments, "arguments.")
+        elif num_arguments == 1:
+            print(num_arguments, "argument:")
+        else:
+            print(num_arguments, "arguments:")
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
+print_arguments()   
