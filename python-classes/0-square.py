43575b62e05cb0c0a67f70ab53319b5ca930372a
Hiskiyas <hiskiyasf@gmail.com>
2023-07-31 16:13:54 +0300

    Documentation Inserted In the console

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,13 +1,43 @@
-#!/usr/bin/python3
 class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        size (int): The size of the square.
+    """
+
     def __init__(self, size):
+        """
+        Initializes a square object.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
     
     def get_size(self):
+        """
+        Retrieves the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
         return self.__size
     
     def set_size(self, size):
+        """
+        Sets the size of the square.
+
+        Args:
+            size (int): The new size of the square.
+        """
         self.__size = size
     
     def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
         return self.__size * self.__size
