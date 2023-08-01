4fd0b0a5ee3d0988c6917c9339a5a454999907b4
Hiskiyas <hiskiyasf@gmail.com>
2023-08-01 12:16:36 +0300

    Doc String to Access and update private attribute

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This class Square defines a square based on 2-square.py
+   using a private instance attribute :size
+   property def size(self) : to retrieve it
+   property etter def size(self, value): to set it
+"""
 class Square:
     """
     This class represents a square.
