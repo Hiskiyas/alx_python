e3eb6e4544bf460c894947290a024ce648fe2dea
Hiskiyas <hiskiyasf@gmail.com>
2023-08-03 16:39:43 +0300

    10:33 Trailing Space Removed

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -7,7 +7,7 @@
 """
 
 
-def inherits_from(obj, a_class): 
+def inherits_from(obj, a_class):
     """
     Checks if an object is an instance of a class that inherited
     (directly or indirectly) from the specified class.
