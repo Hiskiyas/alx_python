c2e0b15a35df8a6e7fbd7b886e44de6da6ded399
Hiskiyas <hiskiyasf@gmail.com>
2023-08-08 11:57:15 +0300

    1. First Rectangle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""
+This class imports a base from the file base.py and 
+takes the main characteristics of the base class.
+"""
+from models.base import Base
+
+class Rectangle(Base):
+    """
+    This class represents a rectangle and inherits from the Base class.
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initializes a rectangle object with width, height, position, and id.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int, optional): The x-coordinate of the rectangle's position.
+            Defaults to 0.
+            y (int, optional): The y-coordinate of the rectangle's position.
+            Defaults to 0.
+            id (int, optional): The id value. Defaults to None.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Getter for width attribute."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for width attribute."""
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for height attribute."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for height attribute."""
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter for x attribute."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter for x attribute."""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter for y attribute."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter for y attribute."""
+        self.__y = value
