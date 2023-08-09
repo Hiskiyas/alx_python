77e19a36542ff5423a81745eabb4249e5a83b9cb
Hiskiyas <hiskiyasf@gmail.com>
2023-08-09 10:26:07 +0300

    1. Response header value #1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -6,12 +6,37 @@ and it displays the value of the variable X-Request-Id.
 import requests
 import sys
 
-url = sys.argv[1]
+def fetch_request_id(url):
+    """
+    Fetches the value of the X-Request-Id variable from
+    the response header of a given URL.
 
-response = requests.get(url)
+    Args:
+        url (str): The URL to send the request to.
 
-if response.status_code == 200:
-    request_id = response.headers.get('X-Request-Id')
-    print(f"X-Request-Id: {request_id}")
-else:
-    print("Error:", response.status_code)
+    Returns:
+        str: The value of the X-Request-Id variable from
+        the response header.
+
+    Raises:
+        requests.exceptions.RequestException: If an error
+        occurs during the HTTP request.
+    """
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        request_id = response.headers.get('X-Request-Id')
+        return request_id
+    else:
+        raise requests.exceptions.RequestException(f"Error: {response.status_code}")
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Please provide a URL as a command-line argument.")
+    else:
+        url = sys.argv[1]
+        try:
+            request_id = fetch_request_id(url)
+            print(f"X-Request-Id: {request_id}")
+        except requests.exceptions.RequestException as e:
+            print(e)
